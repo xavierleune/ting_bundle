4edd22e3df8fba8a8f0a1a6985fb09fc95729fb7
Xavier Leune <xleune@commentcamarche.net>
2014-11-20 09:35:32 +0100

    Declaration of optional service DriverLogger

--- src/TingBundle/DependencyInjection/TingExtension.php
+++ src/TingBundle/DependencyInjection/TingExtension.php
@@ -3,7 +3,10 @@
 namespace CCMBenchmark\TingBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 
@@ -20,6 +23,24 @@ class TingExtension extends Extension
 
         $container->setParameter('ting.repositories', $config['repositories']);
         $container->setParameter('ting.connections', $config['connections']);
+
+        // Adding optional service ting_driverlogger
+        if ($container->hasParameter('ting_driverlogger_class') === true) {
+            $definition = new Definition($container->getParameter('ting_driverlogger_class'));
+            $definition->addArgument(new Reference('logger', ContainerInterface::NULL_ON_INVALID_REFERENCE));
+            $definition->addArgument(new Reference('debug.stopwatch', ContainerInterface::NULL_ON_INVALID_REFERENCE));
+            $container->setDefinition('ting_driverlogger', $definition);
+
+            $reference = new Reference('ting_driverlogger');
+
+            // Add logger to connection Pool
+            $definition = $container->getDefinition('ting_connectionpool');
+            $definition->addArgument($reference);
+
+            // Add logger to DataCollector
+            $definition = $container->getDefinition('ting_data_collector');
+            $definition->addMethodCall('setLogger', [$reference]);
+        }
 
         $servers = $config['memcached']['servers'];
         $config['memcached']['servers'] = array_values($servers);
